2951568804a4bfec23fc1b5e34ca2f494fe83e8c
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-25 18:27:08 +0100

    Add

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'products',
     'users',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
@@ -125,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://www.crafitori.com",
+    "https://crafitori.com",
+    ]
