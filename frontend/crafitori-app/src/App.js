232ee98b33d528e96da968450e5bdfc13f3d9e3f
smoxhakim <joulal1994@gmail.com>
2024-08-26 09:38:24 +0100

    loginPage and homePage

--- frontend/crafitori-app/src/App.js
+++ frontend/crafitori-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/variables.css';
 import SignUp from './pages/signUp';
+import Login from './pages/login';
+import HomePage from './pages/homePage'
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- frontend/crafitori-app/src/pages/login.js
+++ frontend/crafitori-app/src/pages/login.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/signUp.css';
+import '../styles/login.css';
 
-const SignUp = () => {
+const Login = () => {
   const navigate = useNavigate(); // Initialize useNavigate
 
 
   // State for form inputs
   const [username, setName] = useState('');
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirm_password, setConfirmPassword] = useState('');
 
   // State for handling errors and success messages
   const [error, setError] = useState('');
@@ -21,33 +19,35 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (password !== confirm_password) {
-      setError('Passwords do not match');
-      return;
-    }
-
     try {
-      await axios.post('http://localhost:8000/api/users/register/', {
+        const response = await axios.post('http://localhost:8000/api/users/login/', {
         username,
-        email,
         password,
-        confirm_password
       });
 
+        // Extract JWT from the response
+        const { token } = response.data;
+
+        if (token) {
+          // Store the token in localStorage or sessionStorage
+          localStorage.setItem('authToken', token);
+
       // Handle successful registration
-      setSuccess('Registration successful!');
+      setSuccess('Login successful!');
       setError('');
+      console.log(token)
       // Redirect to home page after successful registration
       navigate('/home');
-
-      // Optionally, redirect to another page
-      // window.location.href = '/some-page';
+    } else {
+        setError('Login failed. No token received.');
+        setSuccess('');
+    }
 
     } catch (err) {
       // Handle errors
-      setError('Registration failed. Please try again.');
+      setError('Login failed. Please check your credentials and try again.');
       setSuccess('');
-      console.error('Registration error:', err);
+      console.error('login error:', err);
     }
   };
 
@@ -55,7 +55,7 @@ const SignUp = () => {
     <div className="image-container" alt="Background">
       <div className="signup-container">
         <div className="form-container">
-          <h2>Create An Account!</h2>
+          <h2>welcome back!</h2>
           {error && <p className="error">{error}</p>}
           {success && <p className="success">{success}</p>}
           <form onSubmit={handleSubmit}>
@@ -69,16 +69,6 @@ const SignUp = () => {
               required
             />
 
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-
             <label htmlFor="password">Password</label>
             <input
               type="password"
@@ -89,30 +79,17 @@ const SignUp = () => {
               required
             />
 
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input
-              type="password"
-              id="confirm-password"
-              placeholder="Confirm your password"
-              value={confirm_password}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
+            <p className="forget_password">
+            <a href="/forget-password">forget password</a>
+            </p>
 
-            <div className="terms">
-              <input type="checkbox" id="terms" required />
-              <label htmlFor="terms">
-                I agree to the <a href="/terms">terms of use</a> and <a href="/privacy">privacy policy</a>
-              </label>
-            </div>
-
-            <button type="submit" className="signup-btn">Sign Up</button>
+            <button type="submit" className="signup-btn">Login</button>
           </form>
-          <p>Already have an account? <a href="/login">Login</a></p>
+          <p>Don’t have an account?  <a href="/sign-up">Sign Up</a></p>
         </div>
       </div>
     </div>
   );
 };
 
-export default SignUp;
+export default Login;
