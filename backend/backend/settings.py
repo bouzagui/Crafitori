ea050ffc79bebb000386dfe5b6f6b4ccefa8ede5
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-09-03 22:02:41 +0100

    Extend_access_token_to_1_year

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,8 +58,8 @@ REST_FRAMEWORK={
 
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
