9d6c1438608f6c9bde660c466234960e7daf8a64
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2025-02-09 10:24:43 +0000

    Add domain name to ALLOWED_HOSTS, add requests to requirements.txt and update init.sh to include some neccesary packages

--- backend/init.sh
+++ backend/init.sh
@@ -1,3 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y python3-pip python3-venv nginx
+sudo apt install -y curl software-properties-common ca-certificates lsb-release
 sudo apt install gunicorn
+
+
+#Nodejs
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
